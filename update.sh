ddeaff2d3691779c120b2b0b42f61b3019a5bf38
Victor <victormlndev@gmail.com>
2017-01-23 02:00:44 +0100

    Añadida mejora de busqueda de actualizaciones (solo busca una vez al dia)

--- update.sh
+++ update.sh
@@ -21,72 +21,77 @@
 
 #  Descripción: Comprueba si el script está a la ultima version
 
-# Compruebo que sistema está usando para hacer ping
-# Si es Linux o Mac
-if [ "$(uname -s)" == "Linux" ] || [ "$(uname)" == "Darwin" ]; then
-    ping -c 1 8.8.8.8 &> /dev/null
-    has_internet=$(echo $?)
-    # Si es Windows
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ] ||
-  [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    ping -n 1 www.google.com > /dev/null
-    has_internet=$(echo $?)
-fi
-
-# Si el ping se ha realizado correctamente es que tiene internet
-# por lo que se buscaran actualizaciones
-if [ $has_internet -eq 0 ]
+today=$(date +%Y-%m-%d)
+if [[ "$today" > "$lastChecked" ]]
 then
-  # Si están activadas las actualizaciones automáticas
-  if $search_ota
-  then
-		tieneUltimaVersion=false
-		# Conseguimos la ultima version que hay en github y le quitamos los puntos
-		ultimaVersion=$(curl -s https://raw.githubusercontent.com/victormln/alias/master/user.conf | tail -1 | cut -d'=' -f 2) > /dev/null
-		ultimaVersionSinPuntos=$( echo $ultimaVersion | tr -d ".")
-		# Miramos que versión tiene el usuario actualmente
-		versionActualSinPuntos=$(cat user.conf | tail -1 | cut -d'=' -f 2 | tr -d ".")
-		# Comprobamos si la versionActual es igual o mas grande que la ultimaVersion
-		# es igual a la versionActual.
-		if (( $versionActualSinPuntos>=$ultimaVersionSinPuntos ))
-		then
-			tieneUltimaVersion=true
-		else
-			# Mostramos el mensaje de que hay una nueva actualización
-			echo "###########################################"
-			echo -e "${WARNING}¡NUEVA ACTUALIZACIÓN!${NC}"
-			echo "Tienes la versión: $version"
-			echo "Versión disponible: $ultimaVersion"
-			echo "###########################################"
-			# Si tiene las actualizaciones automaticas, no se le pide nada
-			if $automatic_update
-			then
-				# Si es así, hacemos un pull y le actualizamos el script
-				echo "Hay una nueva actualización y tienes activadas las descargas automáticas."
-				git pull | tee >(echo "Actualizando... Por favor, espere ...")
-				echo -e "${OK}[OK] ${NC}La actualización ha acabado, por favor, vuelva a iniciar el script.";
-			else
-			  echo "Hay una nueva versión de este script y se recomienda actualizar."
-			  echo "Quieres descargarla y así tener las últimas mejoras? y/n o s/n"
-			  # Preguntamos si quiere actualizar
-			  read actualizar
-			  if [ $actualizar == "s" ] || [ $actualizar == "y" ]
-			  then
-			    # Si es así, hacemos un pull y le actualizamos el script
-			  	git pull | tee >(echo "Actualizando... Por favor, espere ...")
-					echo -e "${OK}[OK] ${NC}La actualización ha acabado, por favor, vuelva a iniciar el script.";
-          exit
-			  else
-			    # En el caso que seleccione que no, muestro un mensaje.
-			    echo -e "${WARNING}¡AVISO!${NC} NO se actualizará (aunque se recomienda)."
-					echo -e "**************************"
-					# Damos por su puesto que tiene la ultima version,
-					# para que el script no entre en bucle
-					tieneUltimaVersion=true
-			  fi
-			fi
-		fi
+  # Compruebo que sistema está usando para hacer ping
+  # Si es Linux o Mac
+  if [ "$(uname -s)" == "Linux" ] || [ "$(uname)" == "Darwin" ]; then
+      ping -c 1 8.8.8.8 &> /dev/null
+      has_internet=$(echo $?)
+      # Si es Windows
+  elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ] ||
+    [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+      ping -n 1 www.google.com > /dev/null
+      has_internet=$(echo $?)
   fi
-else
-	echo -e "${WARNING}[AVISO] ${NC}No tienes internet. Para buscar actualizaciones se necesita internet."
+
+  # Si el ping se ha realizado correctamente es que tiene internet
+  # por lo que se buscaran actualizaciones
+  if [ $has_internet -eq 0 ]
+  then
+    # Si están activadas las actualizaciones automáticas
+    if $search_ota || [ "$1" == "--update" ]
+    then
+      lastChecked=$(date +%Y-%m-%d)
+  		tieneUltimaVersion=false
+  		# Conseguimos la ultima version que hay en github y le quitamos los puntos
+  		ultimaVersion=$(curl -s https://raw.githubusercontent.com/victormln/alias/master/user.conf | tail -1 | cut -d'=' -f 2) > /dev/null
+  		ultimaVersionSinPuntos=$( echo $ultimaVersion | tr -d ".")
+  		# Miramos que versión tiene el usuario actualmente
+  		versionActualSinPuntos=$(cat user.conf | tail -1 | cut -d'=' -f 2 | tr -d ".")
+  		# Comprobamos si la versionActual es igual o mas grande que la ultimaVersion
+  		# es igual a la versionActual.
+  		if (( $versionActualSinPuntos>=$ultimaVersionSinPuntos ))
+  		then
+  			tieneUltimaVersion=true
+  		else
+  			# Mostramos el mensaje de que hay una nueva actualización
+  			echo "###########################################"
+  			echo -e "${WARNING}¡NUEVA ACTUALIZACIÓN!${NC}"
+  			echo "Tienes la versión: $version"
+  			echo "Versión disponible: $ultimaVersion"
+  			echo "###########################################"
+  			# Si tiene las actualizaciones automaticas, no se le pide nada
+  			if $automatic_update
+  			then
+  				# Si es así, hacemos un pull y le actualizamos el script
+  				echo "Hay una nueva actualización y tienes activadas las descargas automáticas."
+  				git pull | tee >(echo "Actualizando... Por favor, espere ...")
+  				echo -e "${OK}[OK] ${NC}La actualización ha acabado, por favor, vuelva a iniciar el script.";
+  			else
+  			  echo "Hay una nueva versión de este script y se recomienda actualizar."
+  			  echo "Quieres descargarla y así tener las últimas mejoras? y/n o s/n"
+  			  # Preguntamos si quiere actualizar
+  			  read actualizar
+  			  if [ $actualizar == "s" ] || [ $actualizar == "y" ]
+  			  then
+  			    # Si es así, hacemos un pull y le actualizamos el script
+  			  	git pull | tee >(echo "Actualizando... Por favor, espere ...")
+  					echo -e "${OK}[OK] ${NC}La actualización ha acabado, por favor, vuelva a iniciar el script.";
+            exit
+  			  else
+  			    # En el caso que seleccione que no, muestro un mensaje.
+  			    echo -e "${WARNING}¡AVISO!${NC} NO se actualizará (aunque se recomienda)."
+  					echo -e "**************************"
+  					# Damos por su puesto que tiene la ultima version,
+  					# para que el script no entre en bucle
+  					tieneUltimaVersion=true
+  			  fi
+  			fi
+  		fi
+    fi
+  else
+  	echo -e "${WARNING}[AVISO] ${NC}No tienes internet. Para buscar actualizaciones se necesita internet."
+  fi
 fi
