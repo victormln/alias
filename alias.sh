b81aecfe5153c6e76dfb75f950f482f2daf95917
victormln <victor.molinaf@gmail.com>
2019-12-01 23:48:50 +0100

    Updating version

--- alias.sh
+++ alias.sh
@@ -2,7 +2,7 @@
 # Filename: alias.sh
 # Author: Víctor Molina Ferreira (github.com/victormln)
 # Creation date: 12/11/16
-# Version: 2.1.5
+# Version: 2.1.6
 
 # Mensajes de color
 ERROR='\033[0;31m'
