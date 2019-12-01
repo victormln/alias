97ea49618223580478b4a39ff522d57f6046af54
victormln <victor.molinaf@gmail.com>
2019-12-01 23:34:48 +0100

    Removed unnecesary condition

--- update.sh
+++ update.sh
@@ -54,17 +54,12 @@ if [[ "$TODAY" > "$LAST_UPDATE_CHECKED_IN" ]] || [ "$1" == "--update" ]; then
     last_version_without_dots=$(echo "$last_version" | tr -d ".")
     actual_version_without_dots=$(echo "$CURRENT_VERSION" | tr -d ".")
     if ((actual_version_without_dots >= last_version_without_dots)); then
-      if [ "$1" == "--update" ]; then
-        echo -e "$NO_UPDATES_AVAILABLE"
-        exit 11
-      fi
-
       echo "###########################################"
       echo -e "$NEW_UPDATE_MESSAGE${NC}"
       echo "$YOU_HAVE_VERSION_MESSAGE: $CURRENT_VERSION"
       echo "$AVAILABLE_VERSION_MESSAGE: $last_version"
       echo "###########################################"
-      
+
       if $AUTOMATIC_UPDATE; then
         echo "$AVAILABLE_VERSION_MESSAGE"
         git stash >/dev/null
